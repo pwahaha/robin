88f00ad2439bbfdbec0d03df1b7a4cef8c13156b
Pwahaha <34077604+pwahaha@users.noreply.github.com>
2017-11-29 13:14:55 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,5 +29,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'robin is bad') {
+    	message.send("no u");
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

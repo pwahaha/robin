771402553dccc987de0c3fbc4f8627d891b8f990
Pwahaha <34077604+pwahaha@users.noreply.github.com>
2017-11-29 12:50:57 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+var commands = {
+    ping: 'pong',
+    skills: '{file: "passives.png"}',
+    weapons: '{file: "weapons.png"}',
+    stats: '{file: "stats.png"}'
+    assist: 'Robin does not have any assist skills, you fool.','
+};
+
+client.on('message', message => {
+    if (message.content.substr(0, 1) !== '.') {
+        message.channel.send;
+    }
+    for (var i in commands) {
+        if (message.content === '.' + i) {
+            break;
+        }
+    }
+    message.reply(i);
+});
+
+client.login(process.env.BOT_TOKEN);

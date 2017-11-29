382b3e80680f9db5b7a44e4e3fd4f28937e8dc40
Pwahaha <34077604+pwahaha@users.noreply.github.com>
2017-11-29 13:05:16 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,25 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-var commands = {
-    ping: 'pong',
-    skills: '{file: "passives.png"}',
-    weapons: '{file: "weapons.png"}',
-    stats: '{file: "stats.png"}'
-    assist: 'Robin does not have any assist skills, you fool.','
-};
-
 client.on('message', message => {
-    if (message.content.substr(0, 1) !== '.') {
-        return;
-    }
-    for (var i in commands) {
-        if (message.content === '.' + i) {
-            break;
-        }
-    }
-    message.reply(commands[i]);
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
